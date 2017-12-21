b7e42b8d1c109d8736628744acc5530f5b92f5b6
ltrodl@gmail.com <ltrodl@gmail.com>
2017-12-21 17:29:21 +0900

    2017.12.21 - 거의 다 됨

--- Managing_Academies_upgraded/Curriculums/Result/Result.py
+++ Managing_Academies_upgraded/Curriculums/Result/Result.py
@@ -1,71 +1,64 @@
 import sys
-import pickle
-import os
+# import pickle
+# import os
 sys.path.append("..\\..\\..\\Managing_Academies_upgraded")
 import Make_Academy_class
 import Curriculums.Make_Curiculum_class
 
+print("Managing_Academies에 오신것을 환영합니다.\n하단의 메뉴 중 원하는 동작을 선택하십시오.")
 while 1 :
-    print("Managing_Academies에 오신것을 환영합니다.\n하단의 메뉴 중 원하는 동작을 선택하십시오.")
     order_menu = \
-input("1:학원(과정)정보 생성 2:학원(과정)정보 조회 3:학원(과정)정보 변경 4:학원(과정)정보 삭제 5:종료 : ")
-    if order_menu == '5' : break
+input("1:학원(강의)정보생성 2:학원(강의)정보조회 3:학원(강의)정보수정&삭제 4:종료 : ")
+    if order_menu == '4' : break
     elif order_menu == '1' :
-        order_create = input("1:학원생성 2:과정생성")
-        if order_create == '1' :
-            while 1 :
-                print("아래를 따라 학원 정보를 입력하십시오. 미 기재시 입력 동작이 재실행 됩니다.")
-                academy_name = input("학원 이름을 입력해 주십시오. : ")
-                academy_number = input("학원 전화번호를 입력해 주십시오. : ")
-                academy_address = input("학원 주소를 입력해 주십시오. : ")
-                academy_web_address = input("학원 웹페이지를 입력해 주십시오. : ")
-                academy_curriculums = input("학원의 커리큘럼을 입력해 주십시오.(공백으로 구분) : ")
-                try :
-                    temp_academy_info = Make_Academy_class.academy_information(\
-academy_name, academy_number, academy_address, academy_web_address,academy_curriculums)
-                    Make_Academy_class.make_academy(temp_academy_info)
-                except : continue
-                judge = input("더 입력하시겠습니까?(1:예 2:아니오")
-                if judge == '2' : break
-                elif judge == '1' : continue
-        elif order_create == '2' :
-            while 1 :
-                curriculum_info_tutors = {}
-                curriculum_info_students = {}
-                print("아래를 따라 학원 정보를 입력하십시오. 미 기재시 혹은 오류 입력 시 동작이 재실행 됩니다.")
-                curriculum_academy_name = input("과정의 학원 이름을 입력하십시오. : ")
-                curriculum_name = input("과정명을 입력하십시오. : ")
-                curriculum_time = input("과정 수업 시간을 입력하십시오. : ")
-                curriculum_info_tutors_input = input("강사 정보를 입력하세요.\n\
-e.g)김이박 010-2777-4444 최정강 010-7777-8888 조윤장 010-4562-7894\n강사명 전화번호 : ").split(' ')
-                x = 0
-                while x < len(curriculum_info_tutors_input) :
-                    try : curriculum_info_tutors[curriculum_info_tutors_input[x]] = curriculum_info_tutors_input[x+1]
-                    except:
-                        print("강사 정보를 잘 못 입력하셨습니다.")
-                        x = -1
-                        break
-                    x = x+2
-                if x == -1 : continue
-                curriculum_info_students_input = input("수강생 정보를 입력하세요.\n\
-e.g)김이박 010-2777-4444 최정강 010-7777-8888 조윤장 010-4562-7894\n학생명 전화번호 : ").split(' ')
-                x = 0
-                while x < len(curriculum_info_students_input) :
-                    try : curriculum_info_students[curriculum_info_students_input[x]] = curriculum_info_students_input[x+1]
-                    except:
-                        print("수강생 정보를 잘 못 입력하셨습니다.")
-                        x = -1
-                        break
-                    x = x+2
-                if x == -1 : continue
-                try :
-                    curriculum_info = Curriculums.Make_Curiculum_class.curriculum_information(\
-curriculum_name, curriculum_info_tutors, curriculum_time, curriculum_info_students)
-                    Curriculums.Make_Curiculum_class.update_curriculum(\
-curriculum_academy_name, curriculum_info)
-                except : continue
-
-
+        while 1:
+            order_create = input("1:학원생성 2:강의생성 3:메인메뉴로 돌아가기 : ")
+            if order_create == '3': break
+            elif order_create == '1' :
+                while 1 :
+                    Make_Academy_class.make_academy(Make_Academy_class.make_academy_info())
+                    judge = input("더 입력하시겠습니까?(1:예 2:아니오) : ")
+                    if judge == '2' : break
+                    elif judge == '1' : continue
+            elif order_create == '2' :
+                while 1 :
+                    curriculum_academy_name = input("학원 이름을 입력하십시오. : ")
+                    Curriculums.Make_Curiculum_class.make_curriculum(curriculum_academy_name,\
+Curriculums.Make_Curiculum_class.make_curriculum_info())
+                    print("학원의 강의정보에 해당하는 부분을 수정하십시오.")
+                    Make_Academy_class.change_one_academy(curriculum_academy_name)
+                    judge = input("더 입력하시겠습니까?(1:예 2:아니오) : ")
+                    if judge == '2': break
+                    elif judge == '1': continue
+    elif order_menu == '2' :
+        while 1 :
+            order_search_and_print = input("1:학원전체정보출력 2:학원명만출력 3:학원찾기 4:강의전체정보출력 \
+5:강의명만출력 6:강의찾기 7:메인메뉴로가기 : ")
+            if order_search_and_print == '7' : break
+            elif order_search_and_print == '1' : Make_Academy_class.print_all_academies_info()
+            elif order_search_and_print == '2' : Make_Academy_class.print_all_academies_name()
+            elif order_search_and_print == '4' : Curriculums.Make_Curiculum_class.print_all_curriculmums_info()
+            elif order_search_and_print == '5' : Curriculums.Make_Curiculum_class.print_all_curriculums_name()
+            elif order_search_and_print == '3' :
+                searching_academy_name = input("찾으시려는 학원 이름을 입력해 주십시오. : ")
+                Make_Academy_class.search_academy(searching_academy_name)
+            elif order_search_and_print == '6' :
+                searching_curriculum_name = input("찾으시려는 강의 이름을 입력해 주십시오. : ")
+                Curriculums.Make_Curiculum_class.search_curriculum(searching_curriculum_name)
+    elif order_menu == '3' :
+        while 1 :
+            order_change = input("1:전체학원정보수정 2:특정학원정보수정&삭제 3:강의정보수정&삭제 \
+4:메인메뉴로가기 : ")
+            if order_change == '4' : break
+            elif order_change == '1' : Make_Academy_class.change_all_academies_info()
+            elif order_change == '2' :
+                changing_academy_name = input("수정&삭제하려는 학원이름을 입력하십시오. : ")
+                Make_Academy_class.change_one_academy(changing_academy_name)
+            elif order_change == '3' :
+                changing_curriculum_info = input("변경&삭제 학원이름과 강의명을 입력하십시오.\n\
+e.g)한국it교육원 Python\n학원이름 강의명 : ").split(' ')
+                Curriculums.Make_Curiculum_class.update_curriculum\
+(changing_curriculum_info[0], changing_curriculum_info[1])
 
 
 
